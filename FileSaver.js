792dbdb272e963426b2a8b120a263509a53004b5
nodegin <nodegin@gmail.com>
2015-03-29 17:22:53 +0800

    check features instead of user agent

--- FileSaver.js
+++ FileSaver.js
@@ -20,8 +20,7 @@ var saveAs = saveAs
   || (function(view) {
 	"use strict";
 	// IE <10 is explicitly unsupported
-	if (typeof navigator !== "undefined" &&
-	    /MSIE [1-9]\./.test(navigator.userAgent)) {
+	if (!window.clipboardData || !window.matchMedia) {
 		return;
 	}
 	var
