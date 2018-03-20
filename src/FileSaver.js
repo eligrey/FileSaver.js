a20003093c1b6650202ea12a2096133626165853
Chris Hunt <chrahunt@gmail.com>
2018-03-20 00:47:33 -0400

    Revert interface change.

--- src/FileSaver.js
+++ src/FileSaver.js
@@ -13,7 +13,7 @@
 
 /*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/src/FileSaver.js */
 
-export default var saveAs = saveAs || (function(view) {
+export var saveAs = saveAs || (function(view) {
 	"use strict";
 	// IE <10 is explicitly unsupported
 	if (typeof view === "undefined" || typeof navigator !== "undefined" && /MSIE [1-9]\./.test(navigator.userAgent)) {
