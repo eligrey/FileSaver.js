b590aeeb3958a1baebfaa86000938c64a026e721
Jimmy Wärting <jimmy@warting.se>
2020-11-19 14:32:58 +0100

    Update FileSaver.js

--- src/FileSaver.js
+++ src/FileSaver.js
@@ -69,7 +69,7 @@ function click (node) {
 // Detect WebView inside a native macOS app by ruling out all browsers
 // We just need to check for 'Safari' because all other browsers (besides Firefox) include that too
 // https://www.whatismybrowser.com/guides/the-latest-user-agent/macos
-var isMacOSWebView = /Macintosh/.test(navigator.userAgent) && /AppleWebKit/.test(navigator.userAgent) && !/Safari/.test(navigator.userAgent)
+var isMacOSWebView = _global.navigator && /Macintosh/.test(navigator.userAgent) && /AppleWebKit/.test(navigator.userAgent) && !/Safari/.test(navigator.userAgent)
 
 var saveAs = _global.saveAs || (
   // probably in some web worker
